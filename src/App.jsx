a17a0cd47990fe9fcf05b31932910ec3b6088939
Moose as a Service <endorfinized@users.noreply.github.com>
2026-03-04 02:20:31 -0800

    fix: move DevOps Tools to bottom of page

--- src/App.jsx
+++ src/App.jsx
@@ -54,6 +54,7 @@ export default function App() {
           <Experience />
           <Education />
           <Clients />
+          <DevOpsTools />
           <Footer />
         </div>
       </div>
